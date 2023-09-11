dd1581e74cc91f12756383b84f0a41a382b89e15
btmdevatish <120098142+btmdevatish@users.noreply.github.com>
2023-09-11 15:41:11 +0530

    Update server.js
    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 //const express = require('express')
 const app = require('./app');
 
-const PORT  = process.env.PORT || 3333;
+const PORT  = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
